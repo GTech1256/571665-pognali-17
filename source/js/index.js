ee172ad02456d15774ca30ba3d266c6694e9ba7a
GTech1256 <pomah156@gmail.com>
2019-08-01 14:57:20 +0300

    -

---
+++ source/js/index.js
@@ -0,0 +1,5 @@
+var menuBurger = document.querySelector('.top-bar__toggle-btn');
+var menuBurger = document.querySelector('.top-bar__toggle-btn');
+
+menuBurger.classList.remove('top-bar__toggle-btn_hidden');
+menuBurger.classList.remove('top-bar__toggle-btn_type_close');
