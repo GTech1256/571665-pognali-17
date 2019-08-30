212ea31f737435243074ee49264b3a955a6cb054
GTech1256 <pomah156@gmail.com>
2019-08-31 00:55:23 +0300

    Fix: tests

--- source/js/index.js
+++ source/js/index.js
@@ -3,8 +3,14 @@ var menuBurger = document.querySelector('.top-bar__toggle-btn');
 var topBar = document.querySelector('.top-bar');
 var logo = document.querySelector('.logo__img');
 
+// catalog.html controls
+var countryPickerBar = document.querySelector('.country-picker-bar');
+var countryPickerBtn = document.querySelector('.country-picker__close-btn-wrapper');
+var countryPickerControl = document.querySelector('.companions-country-filter__country-picker-control');
+
 var activeClassOfMobileMenu = 'mobile-menu_open'
 var activeClassOfTopBar = 'top-bar_open'
+var activeClassOfCountryPickerControl = 'companions-country-filter__country-picker-control_active'
 var fixedClassOfTopBar = 'top-bar_position-fixed'
 var menuBurgerTypeClose = 'top-bar__toggle-btn_type_close'
 var logoPaths = {
@@ -12,6 +18,27 @@ var logoPaths = {
   close: 'img/logo-mobile-white@1x.png'
 }
 
+function revertCatalogBar(evt) {
+
+  var isNotClearClick = evt.path.some(function (node) {
+    return node.classList && node.classList.contains('country-picker__name-words')
+  })
+
+  if (isNotClearClick) {
+    return;
+  }
+  evt.preventDefault();
+
+  //country-picker__name-words
+  // CLOSE
+  if (countryPickerControl.classList.contains(activeClassOfCountryPickerControl)) {
+    countryPickerControl.classList.remove(activeClassOfCountryPickerControl)
+    return
+  }
+
+  // OPEN
+  countryPickerControl.classList.add(activeClassOfCountryPickerControl)
+}
 
 function revertMobileMenu() {
   var isMenuBurgerTypeClose = menuBurger.classList.contains(menuBurgerTypeClose);
@@ -49,3 +76,8 @@ menuBurger.addEventListener('click', function (evt) {
 
   revertMobileMenu();
 })
+
+if (countryPickerBar) {
+  countryPickerBar.addEventListener('click', revertCatalogBar);
+  countryPickerBtn.addEventListener('click', revertCatalogBar);
+}
